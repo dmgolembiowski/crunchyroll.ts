03fb7666175f2acfc750eaa0b78d962cb637a308
Tenpi <37512637+Tenpi@users.noreply.github.com>
2021-03-10 13:54:45 -0500

    fix

--- start.ts
+++ start.ts
@@ -5,7 +5,6 @@ import crunchyroll from "./crunchyroll"
     // const output = await crunchyroll.api.getManga("list_chapter", {page_id: 372172, series_id: 527})
     // const pages = output.pages.map((p) => p.image_url)
     // console.log(pages)
-    const episode = await crunchyroll.episode.get("gabriel dropout 1")
-    const result = crunchyroll.util.parseDest(episode, "mp4", "./videos", "{episodeTitle}")
+    const result = await crunchyroll.util.downloadEpisode("gabriel dropout 1", "./videos")
     console.log(result)
 })()
